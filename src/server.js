c89924990b019e99adb3dec1b1438eae942a04e6
Alex Chen <achen19@mit.edu>
2019-09-23 23:34:46 -0400

    first heroku:

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/static', express.static('public'));
+
+app.get('/', function(req, res, next) {
+  res.sendFile('index.html', { root: 'src/views' });
+});
+
+app.get('/members', function(req, res, next) {
+  res.sendFile('members.html', { root: 'src/views' });
+});
+
+app.get('/about', function(req, res, next) {
+  res.sendFile('about.html', { root: 'src/views' });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
